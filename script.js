d1b6d6408693ac8fdc86030826a12c4a321d5963
Nurfauni Ayub <94003633+Nurfauni27@users.noreply.github.com>
2021-11-12 18:44:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let result=document.getElementById("inputext");
+
+let calculate=(number)=>{
+    result.value+=number;
+}
+
+let Result=()=>{
+    try{
+        result.value=eval(result.value)
+    }
+    catch(err){
+        alert("Enter the Valid Input")
+    }
+}
+
+function clr(){
+    result.value="";
+}
+
+function del(){
+    result.value=result.value.slice(0,-1);
+}
+
+function percentage(){
+    result.value=result.value /100
+}
